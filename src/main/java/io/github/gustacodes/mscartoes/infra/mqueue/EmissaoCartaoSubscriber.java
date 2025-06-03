66727b5a30c3c08c3fea774f0ac3b5e69d8ea0a2
Gustavo Henrique <gustalencarvalho@gmail.com>
2025-06-02 22:06:17 -0300

    feat: configurações de mensageria adicionadas

--- src/main/java/io/github/gustacodes/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
+++ src/main/java/io/github/gustacodes/mscartoes/infra/mqueue/EmissaoCartaoSubscriber.java
@@ -1,15 +1,40 @@
 package io.github.gustacodes.mscartoes.infra.mqueue;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.gustacodes.mscartoes.domain.Cartao;
+import io.github.gustacodes.mscartoes.domain.ClienteCartao;
+import io.github.gustacodes.mscartoes.domain.DadosSolicitacaoEmissaoCartao;
+import io.github.gustacodes.mscartoes.infra.repository.CartaoRepository;
+import io.github.gustacodes.mscartoes.infra.repository.ClienteCartaoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class EmissaoCartaoSubscriber {
+
+    private final CartaoRepository cartaoRepository;
+    private final ClienteCartaoRepository clienteCartaoRepository;
 
     @RabbitListener(queues = "${mq.queues.emissao-cartoes}")
     public void receberSolicitacaoEmissao(@Payload String payload) {
-        System.out.println(payload);
+
+        var mapper = new ObjectMapper();
+        try {
+            DadosSolicitacaoEmissaoCartao dados = mapper.readValue(payload, DadosSolicitacaoEmissaoCartao.class);
+            Cartao cartao = cartaoRepository.findById(dados.getIdCartao()).orElseThrow();
+            ClienteCartao clienteCartao = new ClienteCartao();
+            clienteCartao.setCartao(cartao);
+            clienteCartao.setCpf(dados.getCpf());
+            clienteCartao.setLimite(dados.getLimiteLiberado());
+            clienteCartaoRepository.save(clienteCartao);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
